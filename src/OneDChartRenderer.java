b0cb359b5430c0287d1129911410365ca3878161
Tamannajethani <140835838+Tamannajethani@users.noreply.github.com>
2025-07-29 13:37:16 +0530

    Create OneDChartRenderer.java

---
+++ src/OneDChartRenderer.java
@@ -0,0 +1,109 @@
+import javafx.scene.Node;
+import javafx.scene.chart.*;
+import javafx.scene.layout.StackPane;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OneDChartRenderer implements ChartRenderer {
+    @Override
+    public Node render(File csvFile ,String chartType)
+    {
+        if(chartType.equalsIgnoreCase("Pie Chart"))
+        {
+            return renderPieChart(csvFile);
+        }
+        else if(chartType.equalsIgnoreCase("Histogram"))
+        {
+            return renderHistogramChart(csvFile);
+
+        }
+        return new StackPane();
+    }
+    private PieChart renderPieChart(File file)
+    {
+        List<PieChart.Data> dataList=new ArrayList<>();
+        try(BufferedReader br=new BufferedReader(new FileReader(file)))
+        {
+            String line;
+            boolean isFirstLine=true;
+
+            while ((line= br.readLine()) != null)
+            {
+                if(isFirstLine)
+                {
+                    isFirstLine=false;
+                    continue;
+                }
+                String[] parts=line.split(",");
+
+                if(parts.length>=2)
+                {
+                    String label=parts[0].trim();
+                    double value=Double.parseDouble(parts[1].trim());
+                    dataList.add(new PieChart.Data(label,value));
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        PieChart pieChart=new PieChart();
+        pieChart.setData(javafx.collections.FXCollections.observableArrayList(dataList));
+        pieChart.setTitle("Pie Chart");
+
+        return pieChart;
+    }
+
+    private BarChart<String ,Number> renderHistogramChart(File file)
+    {
+        List<String> labels=new ArrayList<>();
+        List<Double> values=new ArrayList<>();
+
+        try(BufferedReader br=new BufferedReader(new FileReader(file)))
+        {
+            String line;
+            boolean isFirstLine=true;
+
+            while ((line= br.readLine()) != null)
+            {
+                if(isFirstLine)
+                {
+                    isFirstLine=false;
+                    continue;
+                }
+                String[] parts=line.split(",");
+
+                if(parts.length>=2)
+                {
+                     labels.add(parts[0].trim());
+                    values.add(Double.parseDouble(parts[1].trim()));
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        CategoryAxis xAxis=new CategoryAxis();
+        NumberAxis yAxis=new NumberAxis();
+        xAxis.setLabel("Category");
+        yAxis .setLabel("Frequency");
+
+        BarChart<String,Number> histogram=new BarChart<>(xAxis,yAxis);
+        histogram.setTitle("Histogram");
+
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+            for(int i=0;i< labels.size();i++)
+            {
+                series.getData().add(new XYChart.Data<>(labels.get(i),values.get(i) ));
+            }
+        histogram.getData().add(series);
+            return histogram;
+    }
+}
