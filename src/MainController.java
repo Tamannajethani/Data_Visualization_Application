cefd340fe16bf70dfb5441bfcf0b375680791e71
Tamannajethani <140835838+Tamannajethani@users.noreply.github.com>
2025-08-04 00:04:43 +0530

    Update MainController.java

--- src/MainController.java
+++ src/MainController.java
@@ -1,13 +1,15 @@
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.FileChooser;
 import java.io.File;
-
-
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.WritableImage;
+import javax.imageio.ImageIO;
 
 public class MainController {
     @FXML
@@ -21,6 +23,9 @@ public class MainController {
     private StackPane chartArea;
 
     @FXML
+    private Button exportButton;
+
+    @FXML
     private Label uploadStatusLabel;
 
     @FXML
@@ -28,25 +33,24 @@ public class MainController {
     private File uploadedFile;
 
     private final  ChartRenderer oneDRenderer=new OneDChartRenderer();
-
+    private final ChartRenderer twoDRender=new TwoDChartRenderer();
     @FXML
     public void initialize()
     {
-        dataTypeCombo.getItems().addAll("1D Data","2D Data","GeoSpatial Data");
+        exportButton.setVisible(false);
+        dataTypeCombo.getItems().addAll("1D Data","2D Data");
 
         dataTypeCombo.setOnAction(e->{
             chartTypeCombo.getItems().clear();
             switch (dataTypeCombo.getValue())
             {
                 case "1D Data":
-                    chartTypeCombo.getItems().addAll("Pie Chart","Histogram");
+                    chartTypeCombo.getItems().addAll("Pie Chart","Bar Graph");
                     break;
                 case "2D Data":
-                    chartTypeCombo.getItems().addAll("Bar Chart","Line Chart","Scatter Plot");
+                    chartTypeCombo.getItems().addAll("Line Chart","Scatter Plot" ,"Bubble Chart");
                     break;
-                case "GeoSpatial Data":
-                    chartTypeCombo.getItems().add("Map");
-                    break;
+
             }
         });
 
@@ -64,6 +68,36 @@ public class MainController {
             uploadStatusLabel.setText("CSV uploaded successfully: " + file.getName());
         }
     }
+    @FXML
+    private void handleExportButton()
+    {
+        if(chartArea.getChildren().isEmpty())
+        {
+            showAlert("No chart to export.");
+            return;
+        }
+
+        Node chartNode= chartArea.getChildren().get(0);
+        WritableImage image= chartNode.snapshot(null,null);
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save Chart As Image");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PNG Image","*.png"));
+        File file= fileChooser.showSaveDialog(null);
+
+        if(file != null)
+        {
+            try{
+                ImageIO.write(SwingFXUtils.fromFXImage(image,null),"png",file);
+                showInfoAlert("Chart exported to : " + file.getAbsolutePath());
+            }catch (Exception e)
+            {
+                e.printStackTrace();
+                showAlert("Failed to export chart. ");
+            }
+        }
+    }
+
 @FXML
         private void handleVisualizeButton()
         {
@@ -90,19 +124,18 @@ public class MainController {
                     break;
 
                 case "2D Data":
-                    //chartNode=oneDRenderer.render(uploadedFile,chartType);
+                    chartNode= twoDRender.render(uploadedFile,chartType);
                     break;
 
-                case "GeoSpatial Data":
-                   // chartNode=oneDRenderer.render(uploadedFile,chartType);
-                    break;
             }
             chartArea.getChildren().clear();
             if (chartNode!=null)
             {
                 chartArea.getChildren().add(chartNode);
+                exportButton.setVisible(true);
             }
             else {
+                exportButton.setVisible(false);
                 showAlert("Unsupported chart or failed to render");
             }
         }
@@ -114,6 +147,14 @@ private void showAlert(String msg)
     alert.showAndWait();
 }
 
+private void showInfoAlert(String msg)
+{
+    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+    alert.setTitle("Success");
+    alert.setHeaderText("Export Successful");
+    alert.setContentText(msg);
+    alert.showAndWait();
+}
 
 }
 
